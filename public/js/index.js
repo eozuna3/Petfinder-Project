8b302a9bee2eb2fb1c1608149cdb3c205566e5b6
Bailee Dastugue <bailee.dast@gmail.com>
2020-05-08 11:24:50 -0500

    search results images are equivalent sizes, removed carousel features on single static header image

--- public/js/index.js
+++ public/js/index.js
@@ -6,27 +6,27 @@
 var $petFoundList = $("#petFound-list");
 
 // *** Added by SB for Friendly Neighborhood Pet Finder
-var petTypesObject;     //global object with petfinder type object for search
-var $loadPetTypesBtn = $("#loadPetTypes");    // button to load type object from petfinder
-var $searchPetsBtn = $('#searchPets');
+var petTypesObject; //global object with petfinder type object for search
+var $loadPetTypesBtn = $("#loadPetTypes"); // button to load type object from petfinder
+var $searchPetsBtn = $("#searchPets");
 var $loginBtn = $("#login");
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  loadPetTypes: function () {
+  loadPetTypes: function() {
     return $.ajax({
       type: "GET",
       url: "api/loadPetTypes/"
     })
-      .then(function (petTypes) {
+      .then(function(petTypes) {
         console.log("petTypes: ", petTypes);
         petTypesObject = petTypes;
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log("error getting types from PetFinder: ", err);
       });
   },
-  searchPets: function () {
+  searchPets: function() {
     var params = "?type=dog&size=small&coat=long&location=texas";
     return $.ajax({
       headers: {
@@ -36,18 +36,18 @@ var API = {
       url: "api/searchPets/" + params
       // data: JSON.stringify(example)
     })
-      .then(function (petsFound) {
+      .then(function(petsFound) {
         // console.log("petTypes: ", petsFound);
         petsFoundObject = petsFound;
         // console.log(petsFoundObject);
         // location.reload();
       })
-      .catch(function (err) {
+      .catch(function(err) {
         console.log("error getting types from PetFinder: ", err);
       });
   },
-  choosePet: function (petId, customerId) {
-    var bodyObj = { "petId": petId, "customerId": customerId };
+  choosePet: function(petId, customerId) {
+    var bodyObj = { petId: petId, customerId: customerId };
     return $.ajax({
       headers: {
         "Content-Type": "application/json"
@@ -59,14 +59,13 @@ var API = {
   }
 };
 
-
 // ** ADDED BY SB
 // displayPetsFound gets displays pets found in PetFinder and repopulates the list
 // uses global petsFoundObject to populate table
-var displayPetsFound = function () {
+var displayPetsFound = function() {
   var testArray = petsFoundObject.petsFound;
   console.log("testArray", testArray);
-  var $pets = testArray.map(function (val) {
+  var $pets = testArray.map(function(val) {
     var $a = $("<a>")
       .text(val.name)
       .attr("href", "/example/" + val.id);
@@ -92,8 +91,6 @@ var displayPetsFound = function () {
   // });
 };
 
-
-
 // ** ADDED BY SB
 // handleLoadPetTypesBtnClick is called when loadPetTypes button is clicked
 // variable petTypes is loaded with PetFinder types object
@@ -108,11 +105,11 @@ var handleLoadPetTypesBtnClick = function() {
 // ** ADDED BY SB
 // handleSearchPetsBtnClick is called when loadSearch button is clicked
 // variable petsFound is loaded with PetFinder petsFound object
-var handleSearchPetsBtnClick = function (event) {
+var handleSearchPetsBtnClick = function(event) {
   event.preventDefault();
   console.log("loadSearchPets");
 
-  API.searchPets().then(function () {
+  API.searchPets().then(function() {
     displayPetsFound();
     // refreshExamples();
   });
@@ -121,16 +118,15 @@ var handleSearchPetsBtnClick = function (event) {
 // ** ADDED BY SB
 // handleChooseBtnClick is called when a pet's choose button is clicked
 // Sends the petfinder unique ID and customer ID to be stored in chosenPetsDB
-var handleChooseBtnClick = function () {
+var handleChooseBtnClick = function() {
   var idToChoose = $(this)
     .parent()
     .attr("data-id");
   var customerId = 1;
-  API.choosePet(idToChoose, customerId).then(function () {
+  API.choosePet(idToChoose, customerId).then(function() {
     refreshExamples();
   });
 };
-
 
 //
 // ** ADDED BY SB
@@ -140,20 +136,18 @@ $searchPetsBtn.on("click", handleSearchPetsBtnClick);
 $petFoundList.on("click", ".choose", handleChooseBtnClick);
 // $loginBtn.on("click", handleFormLogin);
 
-
-
 // ADDED BY BD
-// var dummyArray = [
-//   "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-// ];
-
 var dummyArray = [
-  "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1503066211613-c17ebc9daef0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-  "https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
 ];
 
-var surveyTaken = true;
+// var dummyArray = [
+//   "https://images.unsplash.com/photo-1497752531616-c3afd9760a11?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//   "https://images.unsplash.com/photo-1503066211613-c17ebc9daef0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//   "https://images.unsplash.com/photo-1504208434309-cb69f4fe52b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+// ];
+
+var surveyTaken = false;
 
 function addCards() {
   for (var i = 0; i < dummyArray.length; i++) {
@@ -163,6 +157,7 @@ function addCards() {
       var image = $("<img>");
       image.attr("src", dummyArray[i]).attr("class", "header");
       newDiv.append(image);
+      $(".caro-controls").hide();
     } else {
       if (i === 0) {
         newDiv.attr("class", "carousel-item active card");
@@ -184,6 +179,7 @@ function addCards() {
       // cardInfo.append(links);
       // cardBody.append(cardInfo);
       newDiv.append(cardBody);
+      addIndicators();
     }
     $(".carousel-inner").append(newDiv);
   }
@@ -191,15 +187,19 @@ function addCards() {
 
 function addIndicators() {
   for (var i = 0; i < dummyArray.length; i++) {
-      if (i === 0) {
-          var listItem = $("<li>").attr("data-target", "#demo").attr("data-slide-to", i).attr("class", "active");
-      }
-      else {
-          var listItem = $("<li>").attr("data-target", "#demo").attr("data-slide-to", i);
-      }
-      $(".carousel-indicators").append(listItem);
+    if (i === 0) {
+      var listItem = $("<li>")
+        .attr("data-target", "#demo")
+        .attr("data-slide-to", i)
+        .attr("class", "active");
+    } else {
+      var listItem = $("<li>")
+        .attr("data-target", "#demo")
+        .attr("data-slide-to", i);
+    }
+    $(".carousel-indicators").append(listItem);
   }
 }
 
 addCards();
-addIndicators();
+
