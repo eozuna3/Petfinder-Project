3fd15feaadc9a391541645d273b1e76e5ac264e4
eozuna3 <eo30239@gmail.com>
2020-05-08 22:27:18 -0500

    Created a basic navigation bar for all the html pages.  Created a home page handlebar html file.  Modified the login handlebar file so that login and signup work through the use of modals.  Changed the htmlRoutes.js file to call the all the handlebar files and thus removed the htmlrouteDev.js file.  I also deleted the signup.handlebars file as it was not needed anymore.  I removed the route to htmlDev.js in the server.js file.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,11 @@
 /* eslint-disable indent */
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
 
-   app.get("/", function(req, res) {
+  app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
-
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -15,8 +14,8 @@ module.exports = function (app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function (
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.render("example", {
@@ -25,9 +24,20 @@ module.exports = function (app) {
     });
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
+  app.get("/login", function(req, res) {
+    res.render("login");
   });
 
+  app.get("/search", function(req, res) {
+    res.render("search");
+  });
+
+  app.get("/homepage", function(req, res) {
+    res.render("homepage");
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
 };
